24c0cbc5eb60df613fed33a0df916c5adda8c7b4
majd khasib <majd.khasib98@gmail.com>
2021-02-04 21:30:41 +0200

    fixed close the menu on menuitem click

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel } from "@ionic/react";
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle } from "@ionic/react";
 import {  addOutline, briefcase, homeOutline, search, shareSocial } from "ionicons/icons";
 import React from "react";
 import { LOCAL_STORAGE_KEY_CASSES_ID } from "../../containers/App";
@@ -25,26 +25,43 @@ React.FC=(props)=>{
       </IonHeader>
       <IonContent >
         <IonList dir="rtl" >
+          <IonMenuToggle>
           <IonItem  routerLink="/Home">
             <IonIcon icon={homeOutline} slot="start"></IonIcon>
             <IonLabel>الصفحة الرئيسية</IonLabel>
           </IonItem>
+          </IonMenuToggle> 
+          <IonMenuToggle>
+
           <IonItem  routerLink="/Search">
             <IonIcon icon={search} slot="start"></IonIcon>
             <IonLabel> ابحث عن حالة</IonLabel>
           </IonItem>
+          </IonMenuToggle> 
+
+          <IonMenuToggle>
+
           <IonItem   routerLink="/caseCreation">
             <IonIcon icon={addOutline}slot="start"></IonIcon>
             <IonLabel> أنشئ حالة </IonLabel>
           </IonItem>
+          </IonMenuToggle> 
+
+          <IonMenuToggle>
+
           <IonItem   routerLink={key}>
             <IonIcon icon={briefcase}slot="start"></IonIcon>
             <IonLabel> تصفح الحالة </IonLabel>
           </IonItem>
+          </IonMenuToggle> 
+          <IonMenuToggle>
+
           <IonItem  routerLink="/Share">
       <IonIcon icon={shareSocial} slot="start"></IonIcon>
       <IonLabel> مشاركة حالتي</IonLabel>
     </IonItem>
+    </IonMenuToggle> 
+
           {/* <IonItem  routerLink="/Unversities">
             <IonIcon icon={schoolOutline} slot="start"></IonIcon>
             <IonLabel>الجامعات والتخصصات</IonLabel>
